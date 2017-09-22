84cbdcda8c49ffa8613f38162e189703d0d738df
Erik Kamp <ekamp@ekamp.group.on>
2017-09-22 11:35:48 -0400

    KW-6: Migrated over to TP Registries to avoid reflection

--- app/src/main/java/weather/ekamp/com/weatherappkotlin/WeatherApplication.kt
+++ app/src/main/java/weather/ekamp/com/weatherappkotlin/WeatherApplication.kt
@@ -5,9 +5,20 @@ import toothpick.smoothie.module.SmoothieApplicationModule
 import toothpick.Toothpick
 import weather.ekamp.com.weatherappkotlin.model.inject.WeatherApplicationModule
 
+import toothpick.configuration.Configuration.forDevelopment
+import toothpick.configuration.Configuration.forProduction
+import toothpick.registries.FactoryRegistryLocator
+import toothpick.registries.MemberInjectorRegistryLocator
+import toothpick.Toothpick.setConfiguration
+
 class WeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        val configuration = if (BuildConfig.DEBUG) forDevelopment() else forProduction()
+        setConfiguration(configuration.disableReflection())
+        FactoryRegistryLocator.setRootRegistry(FactoryRegistry())
+        MemberInjectorRegistryLocator.setRootRegistry(MemberInjectorRegistry())
+
         val appScope = Toothpick.openScope(this)
         appScope.installModules(SmoothieApplicationModule(this), WeatherApplicationModule(this))
     }
